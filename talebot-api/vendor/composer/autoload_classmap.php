8f536e5e970f7b8e7c6d1fe41276bd516b621d38
MaiarHadi <mayarobaid850@gmail.com>
2026-04-12 16:45:42 +0300

    save local changes

--- talebot-api/vendor/composer/autoload_classmap.php
+++ talebot-api/vendor/composer/autoload_classmap.php
@@ -7,8 +7,11 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'App\\Http\\Controllers\\Controller' => $baseDir . '/app/Http/Controllers/Controller.php',
+    'App\\Http\\Controllers\\StoryController' => $baseDir . '/app/Http/Controllers/StoryController.php',
+    'App\\Http\\Requests\\GenerateStoryRequest' => $baseDir . '/app/Http/Requests/GenerateStoryRequest.php',
     'App\\Models\\User' => $baseDir . '/app/Models/User.php',
     'App\\Providers\\AppServiceProvider' => $baseDir . '/app/Providers/AppServiceProvider.php',
+    'App\\Services\\AiStoryService' => $baseDir . '/app/Services/AiStoryService.php',
     'Attribute' => $vendorDir . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
     'Brick\\Math\\BigDecimal' => $vendorDir . '/brick/math/src/BigDecimal.php',
     'Brick\\Math\\BigInteger' => $vendorDir . '/brick/math/src/BigInteger.php',
--- talebot-api/vendor/composer/autoload_static.php
+++ talebot-api/vendor/composer/autoload_static.php
@@ -532,8 +532,11 @@ class ComposerStaticInit53b5d56b3b7e3cbac1713e68c8850f6c
 
     public static $classMap = array (
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
+        'App\\Http\\Controllers\\StoryController' => __DIR__ . '/../..' . '/app/Http/Controllers/StoryController.php',
+        'App\\Http\\Requests\\GenerateStoryRequest' => __DIR__ . '/../..' . '/app/Http/Requests/GenerateStoryRequest.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
+        'App\\Services\\AiStoryService' => __DIR__ . '/../..' . '/app/Services/AiStoryService.php',
         'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Brick\\Math\\BigDecimal' => __DIR__ . '/..' . '/brick/math/src/BigDecimal.php',
         'Brick\\Math\\BigInteger' => __DIR__ . '/..' . '/brick/math/src/BigInteger.php',
